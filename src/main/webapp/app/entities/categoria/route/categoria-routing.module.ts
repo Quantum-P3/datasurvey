e3e9bb1fc51d2ec26d00c29a99f68ee1b84b8677
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-14 19:40:25 -0600

    Eliminar modulo de vista de categorias
    Este modulo no esta en uso. Por lo que es mejor eliminarlo para evitar
    que se acumule mucho codigo muerto.

--- src/main/webapp/app/entities/categoria/route/categoria-routing.module.ts
+++ src/main/webapp/app/entities/categoria/route/categoria-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
 import { CategoriaComponent } from '../list/categoria.component';
-import { CategoriaDetailComponent } from '../detail/categoria-detail.component';
 import { CategoriaUpdateComponent } from '../update/categoria-update.component';
 import { CategoriaRoutingResolveService } from './categoria-routing-resolve.service';
 
@@ -11,14 +10,6 @@ const categoriaRoute: Routes = [
   {
     path: '',
     component: CategoriaComponent,
-    canActivate: [UserRouteAccessService],
-  },
-  {
-    path: ':id/view',
-    component: CategoriaDetailComponent,
-    resolve: {
-      categoria: CategoriaRoutingResolveService,
-    },
     canActivate: [UserRouteAccessService],
   },
   {
