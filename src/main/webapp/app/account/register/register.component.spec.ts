d9b78e1332200b2b9f32eb730badc22bf25c6b06
Mariela Bonilla <mbonilla.guti@gmail.com>
2021-07-19 01:27:51 -0600

    fix test register

--- src/main/webapp/app/account/register/register.component.spec.ts
+++ src/main/webapp/app/account/register/register.component.spec.ts
@@ -63,7 +63,7 @@ describe('Component Tests', () => {
           login: '',
           langKey: 'es',
           name: '',
-          firstName: 'userNormal',
+          firstName: 'normalUser',
           profileIcon: 1,
           isAdmin: 0,
           isGoogle: 0,
