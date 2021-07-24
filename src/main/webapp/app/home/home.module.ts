a5b49c39595062b3120e4e93ee7ae04eedbfc027
Mariela Bonilla <mbonilla.guti@gmail.com>
2021-07-24 15:06:15 -0600

    Revert "Merge branch 'feature/landing-page' into dev"
    This reverts commit 4a792447275d138e8c82e89806368a03fcf7b0fe.

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,10 +4,9 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), FontAwesomeModule],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
