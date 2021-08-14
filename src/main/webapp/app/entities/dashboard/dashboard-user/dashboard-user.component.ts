c00a01c350f9230633231353001ef721173b65f1
Paola <paosanchez30@hotmail.com>
2021-08-13 22:04:50 -0600

    agregacion de reportes por encuesta

--- src/main/webapp/app/entities/dashboard/dashboard-user/dashboard-user.component.ts
+++ src/main/webapp/app/entities/dashboard/dashboard-user/dashboard-user.component.ts
@@ -7,6 +7,8 @@ import { UsuarioExtra } from '../../usuario-extra/usuario-extra.model';
 import { Account } from '../../../core/auth/account.model';
 import { AccountService } from '../../../core/auth/account.service';
 import { UsuarioExtraService } from '../../usuario-extra/service/usuario-extra.service';
+import { faListAlt, faUser, faEye, faStar, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import * as Chartist from 'chartist';
 
 @Component({
   selector: 'jhi-dashboard-user',
@@ -21,6 +23,14 @@ export class DashboardUserComponent implements OnInit {
   cantDraft: number = 0;
   cantPublicas: number = 0;
   cantPrivadas: number = 0;
+  faListAlt = faListAlt;
+  faUser = faUser;
+  faEye = faEye;
+  faStar = faStar;
+  faCalendarAlt = faCalendarAlt;
+  reportsGeneral = false;
+  reportForEncuestas = true;
+  duracion?: number = 0;
 
   isLoading = false;
   encuestas?: IEncuesta[];
@@ -37,6 +47,16 @@ export class DashboardUserComponent implements OnInit {
     this.loadUser();
   }
 
+  cambiarVista() {
+    if (this.reportsGeneral) {
+      this.reportsGeneral = false;
+      this.reportForEncuestas = true;
+    } else if (this.reportForEncuestas) {
+      this.reportsGeneral = true;
+      this.reportForEncuestas = false;
+    }
+  }
+
   loadEncuestas() {
     this.encuestaService.query().subscribe(
       (res: HttpResponse<IEncuesta[]>) => {
@@ -45,12 +65,30 @@ export class DashboardUserComponent implements OnInit {
 
         this.encuestas = tmpEncuestas.filter(e => e.usuarioExtra?.id === this.usuarioExtra?.id);
         this.cantEncuestas = this.encuestas.length;
-        this.cantActivas = tmpEncuestas.filter(e => e.estado === 'ACTIVE').length;
-        this.cantDraft = tmpEncuestas.filter(e => e.estado === 'DRAFT').length;
-        this.cantFinalizadas = tmpEncuestas.filter(e => e.estado === 'FINISHED').length;
-        this.cantPublicas = tmpEncuestas.filter(e => e.acceso === 'PUBLIC').length;
-        this.cantPrivadas = tmpEncuestas.filter(e => e.acceso === 'PRIVATE').length;
+        this.cantActivas = tmpEncuestas.filter(e => e.estado === 'ACTIVE' && e.usuarioExtra?.id === this.usuarioExtra?.id).length;
+        this.cantDraft = tmpEncuestas.filter(e => e.estado === 'DRAFT' && e.usuarioExtra?.id === this.usuarioExtra?.id).length;
+        this.cantFinalizadas = tmpEncuestas.filter(e => e.estado === 'FINISHED' && e.usuarioExtra?.id === this.usuarioExtra?.id).length;
+        this.cantPublicas = tmpEncuestas.filter(e => e.acceso === 'PUBLIC' && e.usuarioExtra?.id === this.usuarioExtra?.id).length;
+        this.cantPrivadas = tmpEncuestas.filter(e => e.acceso === 'PRIVATE' && e.usuarioExtra?.id === this.usuarioExtra?.id).length;
+
+        tmpEncuestas.forEach(encuesta => {
+          const _calificacion = encuesta.calificacion;
+          encuesta.calificacion = Number(_calificacion?.toString().split('.')[0]);
+
+          debugger;
+
+          if (encuesta.fechaFinalizada == null) {
+            this.duracion = -1;
+          } else {
+            this.duracion = encuesta.fechaPublicacion?.diff(encuesta.fechaFinalizada!, 'days');
+          }
+        });
+
+        this.cantPersonas = tmpEncuestas.filter(e => e.calificacion && e.usuarioExtra?.id === this.usuarioExtra?.id).length;
         //cantidad de personas que han completado la encuesta
+
+        this.loadFirstChart();
+        this.loadSecondChart();
       },
       () => {
         this.isLoading = false;
@@ -69,4 +107,24 @@ export class DashboardUserComponent implements OnInit {
 
     this.loadEncuestas();
   }
+
+  loadFirstChart(): void {
+    var dataEstado = {
+      labels: ['ACTIVOS', 'BORRADOR', 'FINALIZADOS'],
+      series: [this.cantActivas, this.cantDraft, this.cantFinalizadas],
+    };
+
+    new Chartist.Pie('#chartEstado', dataEstado);
+  }
+
+  loadSecondChart(): void {
+    var dataAcceso = {
+      labels: ['PÚBLICA', 'PRIVADA'],
+      series: [this.cantPublicas, this.cantPrivadas],
+    };
+
+    new Chartist.Pie('#chartAcceso', dataAcceso);
+  }
+
+  detallesPreguntas(): void {}
 }
