4af9d23ed1d978b046d64f3590e3e846662186af
Mariela Bonilla <mbonilla.guti@gmail.com>
2021-08-15 00:39:51 -0600

    modificaciones en estilos de tienda, entre otros

--- src/main/webapp/app/entities/dashboard/dashboard-user/dashboard-user.component.ts
+++ src/main/webapp/app/entities/dashboard/dashboard-user/dashboard-user.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { IUsuarioEncuesta } from '../../usuario-encuesta/usuario-encuesta.model';
+import { IUsuarioExtra } from '../../usuario-extra/usuario-extra.model';
+import { IUser } from '../../user/user.model';
+import { UsuarioEncuestaService } from '../../usuario-encuesta/service/usuario-encuesta.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UsuarioExtraService } from '../../usuario-extra/service/usuario-extra.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AccountService } from '../../../core/auth/account.service';
+import { HttpResponse } from '@angular/common/http';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'jhi-dashboard-user',
@@ -6,7 +16,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-user.component.scss'],
 })
 export class DashboardUserComponent implements OnInit {
-  constructor() {}
+  usuarioEncuestas?: IUsuarioEncuesta[];
+  usuarioExtra: IUsuarioExtra | null = null;
+  user: IUser | null = null;
 
-  ngOnInit(): void {}
+  constructor(
+    protected usuarioEncuestaService: UsuarioEncuestaService,
+    protected modalService: NgbModal,
+    protected usuarioExtraService: UsuarioExtraService,
+    protected activatedRoute: ActivatedRoute,
+    protected accountService: AccountService,
+    protected router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.accountService.getAuthenticationState().subscribe(account => {
+      if (account !== null) {
+        this.usuarioExtraService.find(account.id).subscribe(usuarioExtra => {
+          this.usuarioExtra = usuarioExtra.body;
+          this.loadAllColaboraciones();
+          if (this.usuarioExtra !== null) {
+            if (this.usuarioExtra.id === undefined) {
+              const today = dayjs().startOf('day');
+              this.usuarioExtra.fechaNacimiento = today;
+            }
+          }
+        });
+      }
+    });
+  }
+
+  loadAllColaboraciones(): void {
+    this.usuarioEncuestaService.query().subscribe((res: HttpResponse<IUsuarioEncuesta[]>) => {
+      const tempUsuarioEncuestas = res.body ?? [];
+      this.usuarioEncuestas = tempUsuarioEncuestas
+        .filter(c => c.usuarioExtra?.id === this.usuarioExtra?.id)
+        .filter(c => c.encuesta?.estado !== 'DELETED');
+    });
+  }
 }
